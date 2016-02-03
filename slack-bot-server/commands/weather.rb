b7b44689b310df3c4a335317d9ca0ad7b068ec77
Luis Novo <lfnovo@gmail.com>
2016-02-03 11:33:21 -0800

    fixed for multi-word locations

--- slack-bot-server/commands/weather.rb
+++ slack-bot-server/commands/weather.rb
@@ -1,11 +1,13 @@
 module SlackBotServer
   module Commands
     class Weather < SlackRubyBot::Commands::Base
-  		match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
+  		match /^How is the weather in (?<location>.*)\?$/ do |client, data, match|
     		client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
   		end
   	end
 
   end
 end
+
+
 
